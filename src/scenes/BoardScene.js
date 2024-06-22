460178cec269ecd037158e51098ee6bf9c2973f2
Davichobits <davidruizdeveloper@gmail.com>
2024-06-22 06:25:22 -0500

    Main code organized with comments

--- src/scenes/BoardScene.js
+++ src/scenes/BoardScene.js
@@ -7,34 +7,34 @@ export class BoardScene extends Scene {
   constructor() {
     super("BoardScene");
   }
+
   init() {
     // Reset points
     this.points = 0;
   }
+
   create() {
+    // input
     this.cursors = this.input.keyboard.createCursorKeys();
 
+    // paddle
     this.paddle = this.physics.add.image(0, 0, 'paddle');
-
-    this.physics.world.checkCollision.down = false
-
     this.paddle.body.immovable = true;
-    // Collisiones bola
-    this.ball = this.physics.add.image(0, 0, 'ball');
-    this.ball.setCollideWorldBounds(true);
 
+    // ball
+    this.ball = this.physics.add.image(0, 0, 'ball');
     this.ball.setBounce(1, 1);
     this.ball.setVelocity(200, 200);
+
+    // Collisions
+    this.physics.world.checkCollision.down = false
+    this.ball.setCollideWorldBounds(true);
+    this.paddle.setCollideWorldBounds(true);
+    this.physics.add.collider(this.paddle, this.ball, this.hitPaddle, null, this);
 
     //  Center the sprite to the picture
     Phaser.Display.Align.In.Center(this.paddle, this.add.zone(200, 570, 400, 600));
     Phaser.Display.Align.In.Center(this.ball, this.add.zone(200, 100, 400, 600));
-
-
-    this.paddle.setCollideWorldBounds(true);
-
-    this.physics.add.collider(this.paddle, this.ball, this.hitPaddle, null, this);
-
 
     // Score
     this.add.text(12, 10, 'Score:', { fontSize: '24px', fontFamily: 'monospace' });
@@ -50,16 +50,13 @@ export class BoardScene extends Scene {
       this.paddle.setVelocityX(400);
     }
 
-
     if (this.ball.y > this.scale.height) {
       this.physics.pause();
       this.ball.setTint(0xff0000);
     }
-
   }
 
   hitPaddle(paddle, ball) {
-
     this.points += 1;
     this.scoreText.setText(this.points);
   }
